f02c8b22e6d394b8e0fb30abd531a40418f535f4
TheSaintUkraine <68438468+TheSaintUkraine@users.noreply.github.com>
2021-08-11 16:46:14 +0300

    Update script.js

--- 1/script.js
+++ 1/script.js
@@ -1,22 +1,33 @@
 function Check(string) {
     let o = 0;
     let z = 0;
-    for (let i = 0; i < string.length; i++) {
-       if(string[i] == "0") {
-           z++;
-       }
-       else {
-           o++;
-       }
-       if(o < z) {
-           return false;
-       }
-    }
-    if(z == o) {
-        return true;
+    if(typeof(string) == "string") {
+        for (let i = 0; i < string.length; i++) {
+            if(string[i] == "0") {
+                z++;
+            }
+            else if (string[i] == "1"){
+                o++;
+            }
+            else {
+                return false;
+            }
+            if(o < z) {
+                return false;
+            }
+         }
+         if(z == o) {
+             return true;
+         }
+         else {
+             return false;
+         }
     }
     else {
         return false;
     }
 }
-console.log(Check("110010"));
+console.log(Check([1,1,1,0,0,0]));
+console.log(Check(111000));
+console.log(Check("111o00"));
+console.log(Check("101010"));
