bddd77f16379a3ed6178748436b54035161879f0
TheSaint20010 <68438468+TheSaintUkraine@users.noreply.github.com>
2021-08-18 14:46:43 +0300

    Update App.js

--- 2/src/App.js
+++ 2/src/App.js
@@ -73,7 +73,7 @@ export default class App extends Component {
         <ul>
         {this.state.history.map((item,i) =>
           
-            <li className={item.color} key={i.toString()}> {item.value} - {item.status}</li>
+            <li style={{color:item.color}} key={i.toString()}> {item.value} - {item.status}</li>
           
          )}
         </ul>
